3b8ee59a5cc09536d087d50da1abdcc2459d61a1
nusratjahan7 <nusratjahan77222@gmail.com>
2026-04-12 10:58:53 +0600

    add treanding apps and data,json

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Banner from "@/components/Homepage/Banner";
 import Stats from "@/components/Homepage/Stats";
+import TrendingApps from "@/components/Homepage/TrendingApps";
 
 export default function Home() {
   return (
     <div>
       <Banner />
       <Stats />
+      <TrendingApps />
     </div>
   );
 }
